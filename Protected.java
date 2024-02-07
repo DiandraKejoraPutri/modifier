e53274b07185b21833c79bc7ea8a6628b09c7a77
DiandraKejoraPutri <156975662+DiandraKejoraPutri@users.noreply.github.com>
2024-02-07 18:44:25 +0700

    Add files via upload

---
+++ Protected.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modifier;
+
+public class Protected {
+    protected String name;
+    
+     public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ Public.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modifier;
+
+class Public {
+    public String name;
+    
+    public void changeName(String newName){
+        this.name = newName;
+    }
+    
+}
